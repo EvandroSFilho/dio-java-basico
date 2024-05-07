655a9e4795e4339ac8a6929bb0c6fdf900a9bd62
Evandro Santana filho <123319087+EvandroSFilho@users.noreply.github.com>
2024-05-07 19:42:58 -0300

    Estudos da aula de Anatomia das Classes

---
+++ java-anatomia-classes/src/edu/evandro/segundasemana/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.evandro.segundasemana;
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+       String primeiroNome = "Evandro";
+       String segundoNome = "Filho";
+
+       String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+       System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
