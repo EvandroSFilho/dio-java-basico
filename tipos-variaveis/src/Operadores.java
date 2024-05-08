a9768558872b6e017ed87a014217dba3e42eaf47
Evandro Santana filho <123319087+EvandroSFilho@users.noreply.github.com>
2024-05-07 22:38:15 -0300

    Conteudos da aula de Tpos e variáveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // String nomeCompleto = "LINGUAGEM" + "JAVA";
+        // System.out.print(nomeCompleto);
+
+        /*
+         * String concatenacao = "?";
+         * 
+         * concatenacao = 1 + 1 + 1 + "1";
+         * System.out.print(concatenacao);
+         * 
+         * concatenacao = 1 + "1" + 1 + 1;
+         * System.out.print(concatenacao);
+         * 
+         * concatenacao = 1 + "1" + 1 + "1";
+         * System.out.print(concatenacao);
+         * 
+         * concatenacao = "1" + 1 + 1 + 1;
+         * System.out.print(concatenacao);
+         * 
+         * concatenacao = "1" + (1 + 1 + 1);
+         * System.out.print(concatenacao);
+         * 
+         * 
+         * 
+         * int numero = 5;
+         * numero = -numero;
+         * System.out.println(numero);
+         * 
+         * numero = numero * -1;
+         * System.out.println(numero);
+         * 
+         * 
+         * int numero = 5;
+         * numero = numero + 1;
+         * 
+         * System.out.println(numero);
+         */
+
+         boolean variavel = false;
+         variavel = !variavel;
+         System.out.println(variavel);
+    }
+}
