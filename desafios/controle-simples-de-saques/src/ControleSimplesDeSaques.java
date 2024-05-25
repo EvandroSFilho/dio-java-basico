b64cd886c9cc0147a8585ced668891215be48bce
Evandro Santana filho <123319087+EvandroSFilho@users.noreply.github.com>
2024-05-24 21:02:59 -0300

    Criando projeto controle simples de saque

---
+++ desafios/controle-simples-de-saques/src/ControleSimplesDeSaques.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o limite diario de saques: ");
+        double limiteDiario = scanner.nextDouble();
+
+        // Loop for para iterar sobre os saques
+        for (int i = 1; i <= limiteDiario; i++) {
+
+            System.out.println("Informe o valor do saque desejado: ");
+            double valorSaque = scanner.nextDouble();
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break; // Encerra o loop
+            } else if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break; // Encerra o loop
+            } else {
+                // TODO: Atualizar o limite diário e imprimir a saída no formato dos exemplos.
+                limiteDiario = limiteDiario - valorSaque;
+                System.out.println(String.format("Saque realizado. Limite restante: %.1f", limiteDiario));
+            }
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
